8f6767adf6c3db913a72e27cf73dd13a22568cbe
wincchesster <wincchesster@gmail.com>
2020-08-19 13:44:11 +0300

    before connect bd

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const cors =require('cors')
+const morgan = require('morgan')
 const authRoutes = require('./routes/auth')
 const analyticsRoutes = require('./routes/analytics')
 const categoryRoutes = require('./routes/category')
 const orderRoutes = require('./routes/order')
 const positionRoutes = require('./routes/position')
-
-
 const app = express()
+
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(cors())
 
 
 app.use('/api/auth', authRoutes)
